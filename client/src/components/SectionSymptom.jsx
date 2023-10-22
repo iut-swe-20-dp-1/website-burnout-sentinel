ff8f58a2d386eb5d2ee1c0f9f212e9dfac0a4ba2
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-10-23 02:33:09 +0600

    landing page symptoms section first commit #2

---
+++ client/src/components/SectionSymptom.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+const SectionSymptom = () => {
+    return (
+        <>
+            <div class="container mx-auto p-4 lg:p-12">
+                <div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
+                    <div class="overflow-hidden rounded-2xl bg-blue-50 p-4 lg:p-12">
+                        <div class="flex items-center text-blue-500">
+                            <p class="text-sm font-bold uppercase">Feature 1 & Feature 2</p>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </div>
+
+                        <h2 class="mt-4 text-3xl font-semibold text-slate-800">This is an Amazing Feature</h2>
+
+                        <p class="mt-4 text-lg text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        <div class="mt-12 flex transform items-center justify-center transition-transform duration-150 ease-in-out hover:scale-125">
+
+                        </div>
+                    </div>
+
+                    <div class="overflow-hidden rounded-2xl bg-pink-50 p-4 lg:p-12">
+                        <div class="flex items-center text-pink-500">
+                            <p class="text-sm font-bold uppercase">Feature 3 & Feature 4</p>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </div>
+
+                        <h2 class="mt-4 text-3xl font-semibold text-slate-800">This is an Amazing Feature</h2>
+
+                        <p class="mt-4 text-lg text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        <div class="mt-12 flex transform items-center justify-center transition-transform duration-150 ease-in-out hover:scale-125">
+
+                        </div>
+                    </div>
+
+                    <div class="overflow-hidden rounded-2xl bg-green-50 p-4 lg:p-12">
+                        <div class="flex items-center text-green-500">
+                            <p class="text-sm font-bold uppercase">Feature 5 & Feature 6</p>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </div>
+
+                        <h2 class="mt-4 text-3xl font-semibold text-slate-800">This is an Amazing Feature</h2>
+
+                        <p class="mt-4 text-lg text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        <div class="mt-12 flex transform items-center justify-center transition-transform duration-150 ease-in-out hover:scale-125">
+
+                        </div>
+                    </div>
+
+                    <div class="overflow-hidden rounded-2xl bg-purple-50 p-4 lg:p-12">
+                        <div class="flex items-center text-purple-500">
+                            <p class="text-sm font-bold uppercase">Feature 7 & Feature 8</p>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </div>
+
+                        <h2 class="mt-4 text-3xl font-semibold text-slate-800">This is an Amazing Feature</h2>
+
+                        <p class="mt-4 text-lg text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        <div class="mt-12 flex transform items-center justify-center transition-transform duration-150 ease-in-out hover:scale-125">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SectionSymptom
