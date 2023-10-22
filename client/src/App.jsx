950dfaa0fee7186e89daeedadd895e16dae47693
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-10-23 01:48:53 +0600

    added landing page to routes #2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
+//dependencies import
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//import css
 import './App.css'
 
+//components import
+import LandingPage from './pages/LandingPage'
 
 function App() {
 
   return (
     <>
-      <div className='text-xl font-bold'>TESTING</div>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<LandingPage />} />
+        </Routes>
+        </BrowserRouter>
     </>
   )
 }
