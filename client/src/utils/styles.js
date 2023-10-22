3495a1982c45274455e59445b97147f756901fef
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-10-23 02:33:32 +0600

    added styles utils #1

---
+++ client/src/utils/styles.js
@@ -0,0 +1,3 @@
+export const navLinkClass = "py-4 lg:mx-5 lg:py-0 relative text-lg w-fit block after:block after:content-[''] text-[#300722] after:absolute after:h-[3px] after:bg-[#7366FF] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-500 after:origin-center hover:text-[#7366FF]"
+
+export const primaryButtonClass = "relative py-1.5 -top-1 -left-1 bg-[#FFFFFF] border-[#300722] font-medium text-xl rounded-xl transition-all duration-300 before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:rounded-xl before:bg-[#FFFFFF] before:w-full before:border-2 before:border-[#7366FF]  before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0"
