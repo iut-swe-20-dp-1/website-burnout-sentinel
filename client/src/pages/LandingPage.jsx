60e4057571aa857d61294440e795c7ff0eaa55dc
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-10-23 20:44:43 +0600

    added about us and features section #2

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,6 +4,9 @@ import React from 'react'
 import PublicNavbar from '../components/PublicNavbar'
 import SectionHero from '../components/SectionHero'
 import SectionSymptom from '../components/SectionSymptom'
+import SectionFeatures from '../components/SectionFeatures'
+import SectionAboutUs from '../components/SectionAboutUs'
+import Footer from '../components/Footer'
 
 const LandingPage = () => {
   return (
@@ -12,8 +15,11 @@ const LandingPage = () => {
         backgroundImage: "url(https://images.unsplash.com/photo-1487147264018-f937fba0c817?auto=format&fit=crop&q=80&w=1374&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
       }}>
         <PublicNavbar />
-        <SectionHero />
+        <SectionHero/>
         <SectionSymptom />
+        <SectionFeatures/>
+        <SectionAboutUs/>
+        <Footer/>
       </div>
     </>
   )
