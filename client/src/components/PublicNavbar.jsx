d919f8e5015378713e822be82d174768f981f043
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-10-23 20:46:03 +0600

    navbar buttons and links linked #2

--- client/src/components/PublicNavbar.jsx
+++ client/src/components/PublicNavbar.jsx
@@ -1,28 +1,32 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Button from './Button'
 import { navLinkClass } from '../utils/styles'
 
 const PublicNavbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const navigate = useNavigate();
+    const redirect_to_login =()=>{
+        navigate('/login');
+    }
     return (
         <>
-            <nav className="bg-white flex justify-between  text-[#300722] m-2 rounded-md">
+            <nav className="bg-white/70 backdrop-blur-md shadow-md flex justify-between w-full fixed top-0 left-0 right-0 z-20  text-[#300722]">
                 <div className="px-5 xl:px-12 py-4 flex w-full items-center">
                     <Link className="text-xl font-bold font-heading flex items-center" to="/">
                         <img className="h-9 mr-4" src="logo.png" alt="logo" />
                         Burnout Sentinel
                     </Link>
                     <ul className={`${isMenuOpen? "hidden" : "block"} lg:flex px-4 mx-auto font-semibold font-heading lg:space-x-6 text-[#300722]`}>
-                        <li><Link className={navLinkClass} to="#home">Home</Link></li>
-                        <li><Link className={navLinkClass} to="">Symptoms</Link></li>
-                        <li><Link className={navLinkClass} to="">Features</Link></li>
-                        <li><Link className={navLinkClass} to="">About</Link></li>
+                        <li><a className={navLinkClass} href="#home">Home</a></li>
+                        <li><a className={navLinkClass} href="#symptoms">Symptoms</a></li>
+                        <li><a className={navLinkClass} href="#features">Features</a></li>
+                        <li><a className={navLinkClass} href="#about_us">About</a></li>
                     </ul>
-                    <div className={`${isMenuOpen? "hidden" : "block"} lg:flex lg:space-x-12`}>
-                        <Link className={`h-full m-auto font-bold text-[#7366FF] ${navLinkClass}`} to="">Register</Link>
+                    <div className={`${isMenuOpen? "hidden" : "block"} lg:flex lg:space-x-4`}>
+                        <Link className={`h-full m-auto font-bold text-[#7366FF] ${navLinkClass}`} to="/register">Register</Link>
 
-                        <Button additional_classes={"lg:px-10 md:px-6 px-6 text-white bg-[#7366FF] "} button_text={"Login"} />
+                        <Button additional_classes={"lg:px-10 md:px-6 px-6 text-white bg-[#7366FF] "} button_text={"Login"} button_function={redirect_to_login}/>
                     </div>
 
                 </div>
