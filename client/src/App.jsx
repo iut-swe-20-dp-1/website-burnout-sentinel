2a2f065a0b322dc5b395b89bf115e7d4ef2abf55
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-10-24 01:44:37 +0600

    added page routes to login and register page #4 #5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import './App.css'
 
 //components import
 import LandingPage from './pages/LandingPage'
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         </Routes>
         </BrowserRouter>
     </>
