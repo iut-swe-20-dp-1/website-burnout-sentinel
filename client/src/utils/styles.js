7ddbfdfd8bea133572f41d7fc2925ab04b40063a
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-10-24 01:43:50 +0600

    added styles for login and register forms #4 #5

--- client/src/utils/styles.js
+++ client/src/utils/styles.js
@@ -1,3 +1,9 @@
 export const navLinkClass = "py-4 lg:mx-5 lg:py-0 relative text-lg w-fit block after:block after:content-[''] text-[#300722] after:absolute after:h-[3px] after:bg-[#7366FF] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-500 after:origin-center hover:text-[#7366FF]"
 
 export const primaryButtonClass = "relative py-1.5 -top-1 -left-1 border-[#300722] font-medium text-xl rounded-xl transition-all duration-300 before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:rounded-xl before:bg-[#FFFFFF] before:w-full before:border-2 before:border-[#7366FF]  before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0"
+
+export const secondaryButtonClass= "text-[#7366FF] bg-[#FFDFDF] border-2 border-white hover:border-[#7366FF] font-bold rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
+
+export const inputFieldClass="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#7366FF]"
+
+export const labelClass ="block uppercase tracking-wide text-[#7366FF] text-sm font-bold mb-2"
