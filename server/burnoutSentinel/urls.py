d8c936b0c6322b4a015565f6e10f30e7e886bef5
Ayesha Afroza Mohsin <ayeshaafroza@iut-dhaka.edu>
2023-10-24 20:27:00 +0600

    signin kind of works but password stored and csrf disabled

--- server/burnoutSentinel/urls.py
+++ server/burnoutSentinel/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.func1),
+    path('signin/', views.signIn),
 ]
--- server/burnoutSentinel/views.py
+++ server/burnoutSentinel/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 import pyrebase
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
 
 # import firebase_admin
 # from firebase_admin import credentials, initialize_app
@@ -26,3 +30,20 @@ firebaseConfig = {
 # var analytics = getAnalytics(app)
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
+
+
+@csrf_exempt
+def signIn(request):
+    print("Well, we're inside the signin function ig")
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        email = data.get('email')
+        password = data.get('password')
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            response = HttpResponse("Successfully signed in", status=200)
+        except Exception as error:
+            response = HttpResponse(error.args[1], status=501)
+        return response
+    else:
+        return HttpResponse("This endpoint requires a POST method", status=405)
