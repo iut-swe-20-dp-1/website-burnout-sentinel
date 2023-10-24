81935a89aaa4c87b4cfffb2e8ff6404be24fe679
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-10-25 01:36:00 +0600

    added route to user homepage in app.jsx #7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import './App.css'
 import LandingPage from './pages/LandingPage'
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import UserHomepage from './pages/UserHomepage';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<UserHomepage />} />
         </Routes>
         </BrowserRouter>
     </>
