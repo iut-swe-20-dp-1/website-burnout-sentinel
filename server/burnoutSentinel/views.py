ad833bae129d1f8ceea51a260f62881f3f551764
Ayesha Afroza Mohsin <ayeshaafroza@iut-dhaka.edu>
2023-10-25 13:28:12 +0600

    Django Backend Setup #8 trying to set firebase admin sdk

--- server/burnoutSentinel/views.py
+++ server/burnoutSentinel/views.py
@@ -5,16 +5,14 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from decouple import config
-
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
 
 
 # Import CORS decorators
 # from django.views.decorators.cors import ensure_csrf_cookie, never_cache
 # from corsheaders.decorators import cors_headers
-
-# config_path = '/path/to/your/.env'
-# config.read(config_path)
-
 
 # Access the values from the .env file
 FIREBASE_API_KEY = config('FIREBASE_API_KEY')
@@ -42,9 +40,13 @@ firebase_config = {
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
 
+# cred = credentials.Certificate('./firebase-sdk.json')
+# firebase_admin.initialize_app()
 
 # @method_decorator(ensure_csrf_cookie, name='dispatch')
 # @method_decorator(never_cache, name='dispatch')
+
+
 @csrf_exempt  # Disable CSRF protection for this view
 def signIn(request):
     if request.method == 'POST':
@@ -53,6 +55,9 @@ def signIn(request):
         password = data.get('password')
         try:
             user = auth.sign_in_with_email_and_password(email, password)
+            print("USER IS : ")
+            print(user)
+            # custom_token = auth.create_custom_token(uid)
             response_data = {"Successfully signed in"}
             return HttpResponse(response_data, status=200)
         except Exception as error:
