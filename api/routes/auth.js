03415c65ca7060d45afb16833924f100e0ea39de
Ayesha Afroza Mohsin <ayeshaafroza@iut-dhaka.edu>
2024-01-14 22:55:14 +0600

    failed google integration

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { authMiddleware } = require("../middleware/auth");
 const passport = require("../config/passport");
+const cors = require("cors");
 
 // Custom middleware to extract the 'register' flag
 const extractRegisterFlag = (req, res, next) => {
+  console.log("extracted flag from middleware");
   req.session.register = req.query.register === "true";
   next();
 };
@@ -16,7 +18,7 @@ const {
   logout,
   googleFailure,
   googleLogin,
-  getTokenForGoogle
+  getTokenForGoogle,
 } = require("../controllers/auth");
 
 router.route("/register").post(register);
@@ -34,13 +36,45 @@ router.get("/googlefailure", googleFailure);
 // Use this to start authentication using google
 router.get(
   "/google",
+  (req, res, next) => {
+    try {
+      console.log("accesed /google");
+      // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+      // res.setHeader("Access-Control-Allow-Credentials", "true");
+      // res.setHeader("Access-Control-Max-Age", "1800");
+      // res.setHeader("Access-Control-Allow-Headers", "content-type");
+      // res.setHeader(
+      //   "Access-Control-Allow-Methods",
+      //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+      // );
+      next();
+    } catch (err) {
+      console.log(err);
+    }
+  },
   extractRegisterFlag,
   passport.authenticate("google", { scope: ["profile", "email"] })
-); 
+);
 
 // This route is accessed after authentication
 router.get(
-  "/google/callback", 
+  "/google/callback",
+  (req, res, next) => {
+    try {
+      console.log("accesed /google/callback");
+      // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+      // res.setHeader("Access-Control-Allow-Credentials", "true");
+      // res.setHeader("Access-Control-Max-Age", "1800");
+      // res.setHeader("Access-Control-Allow-Headers", "content-type");
+      // res.setHeader(
+      //   "Access-Control-Allow-Methods",
+      //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+      // );
+      next();
+    } catch (err) {
+      console.log(err);
+    }
+  },
   passport.authenticate("google", {
     failureRedirect: "/api/auth/googlefailure",
   }),
@@ -49,5 +83,12 @@ router.get(
 
 // Get token for google logins
 router.get("/gettokenforgoogle", getTokenForGoogle);
+
+// Dummy
+router.get("/google/dummy", (req, res) => {
+  const register = req.query.register;
+  console.log("dummy route accessed.");
+  res.redirect(`/api/auth/google?register=${register}`);
+});
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
+
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const passport = require("./config/passport");
 
@@ -17,6 +18,35 @@ try {
 }
 
 const app = express();
+
+// app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Allow-Methods, Access-Control-Request-Headers, Access-Control-Allow-Origin"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
 app.use(
   session({
     secret: process.env.SESSION_KEY,
@@ -33,14 +63,6 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
 
 // Setup Routes
 app.use("/api/auth", require("./routes/auth"));
