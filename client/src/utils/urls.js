dc3085f68c1505627924a7f652bf94b655f0a5f4
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-01-17 22:40:52 +0600

    testing deployment

--- client/src/utils/urls.js
+++ client/src/utils/urls.js
@@ -1,10 +1,11 @@
-export const serverUrl = 'http://localhost:8800'
-// export const serverUrl = 'https://burnout-sentinel.onrender.com'
+// export const serverUrl = 'http://localhost:8800'
+export const serverUrl = 'https://burnout-sentinel.onrender.com'
 
-export const mlUrl = 'http://127.0.0.1:8000'
-// export const mlUrl = 'https://burnout-sentinel-ml.onrender.com'
+// export const mlUrl = 'http://127.0.0.1:8000'
+// export const mlUrl = 'https://serpapi.com/search.json?engine=google&q=ways+to+relieve+academic+stress'
+export const mlUrl = 'https://burnout-sentinel-ml.onrender.com'
 
-export const clientUrl = 'http://localhost:5173'
-// export const clientUrl = 'https://burnout-sentinels.netlify.app'
+// export const clientUrl = 'http://localhost:5173'
+export const clientUrl = 'https://burnout-sentinels.netlify.app'
 
 export const sampleCsvUrl = "https://res.cloudinary.com/dwvhnoeqo/raw/upload/v1705396987/BurnoutSentinel/CSV/Sample_CSV_pcsnyv.CSV"
